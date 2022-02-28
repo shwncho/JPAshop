ca4f4c5984eb121b8cdc4f711e991c173addeceb
simple <shwncho4@gmail.com>
2022-03-01 01:36:11 +0900

    Create Item domain

--- src/main/java/jpabook/jpashop/domain/item/Item.java
+++ src/main/java/jpabook/jpashop/domain/item/Item.java
@@ -1,6 +1,7 @@
 package jpabook.jpashop.domain.item;
 
 import jpabook.jpashop.domain.Category;
+import jpabook.jpashop.exception.NotEnoughStockException;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -25,4 +26,20 @@ public abstract class Item {
 
     @ManyToMany(mappedBy = "items")
     private List<Category> categories = new ArrayList<>();
+
+    //==비즈니스 로직==// setter로 가지고 변경하는게 아니라 메소드로 이렇게해야 가장 객체지향 적인것
+    /*
+    stock 증가
+     */
+    public void addStock(int quantity){
+        this.stockQuantity = quantity;
+    }
+
+    public void removeStock(int quantity){
+        int restStock = this.stockQuantity-quantity;
+        if(restStock<0){
+            throw new NotEnoughStockException("need more stock");
+        }
+        this.stockQuantity = restStock;
+    }
 }

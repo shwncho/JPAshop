f39410666645dd52940eadad28edb3b5791b5af0
simple <shwncho4@gmail.com>
2022-03-01 18:40:00 +0900

    Create Order Test

--- src/main/java/jpabook/jpashop/domain/item/Item.java
+++ src/main/java/jpabook/jpashop/domain/item/Item.java
@@ -32,7 +32,7 @@ public abstract class Item {
     stock 증가
      */
     public void addStock(int quantity){
-        this.stockQuantity = quantity;
+        this.stockQuantity += quantity;
     }
 
     public void removeStock(int quantity){
